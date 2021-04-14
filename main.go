37c49189f540adb372fe3d35cd74cbf327081f3e
Rhys <40720420+maughan@users.noreply.github.com>
2021-04-14 18:12:46 +0100

    Update main.go

--- main.go
+++ main.go
@@ -8,28 +8,6 @@ import (
 const token string = "NzU0MTAwMTEzNTAwMDEyNTY1.X1v0dQ.dIZvtwrl2vScDeSPQgjYS7CL27Q"
 var BotID string
 
-// func bumLogin () {
-//     client := &http.Client{}
-//     req, _ := http.NewRequest("POST", "https://bumble.com/mwebapi.phtml?SERVER_SUBMIT_PHONE_NUMBER", nil)
-//     req.Header.Add("X-Desktop-web", "1")
-//     req.Header.Add("Origin", "https://bumble.com")
-//     req.Header.Add("Referer", "https://bumble.com/get-started")
-//     req.Header.Add("Sec-Fetch-Mode", "cors")
-//     req.Header.Add("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36")
-//     req.Header.Add("x-use-session-cookie", "1")
-//     req.Body = ioutil.NopCloser(strings.NewReader(`{"version":1,"message_type":15,"message_id":12,"body":[{"message_type":15,"server_submit_phone_number":{"remember_me":true,"phone":"' + +447856916071 + '","password":"' + p + '","stats_data":""}}],"is_background":false}`))
-//     resp, _ := client.Do(req)
-//     defer resp.Body.Close()
-//     if resp.StatusCode == http.StatusOK {
-//         bodyBytes, err := ioutil.ReadAll(resp.Body)
-//         if err != nil {
-//             fmt.Println(err.Error())
-//         }
-//         bodyString := string(bodyBytes)
-//         fmt.Println(bodyString)
-//     }
-// }
-
 func main () {
     dg, err := discordgo.New("Bot " + token)
 
